ec5bab8e0182bf37b76012a3f5234db8965ecb18
rykrev <hugo773344@gmail.com>
2024-06-22 16:29:30 -0400

    initialized ui and got operations working

--- script.js
+++ script.js
@@ -1,35 +1,83 @@
-let sessionMath = []
-let number1;
-let number2; 
-let operator; 
-let displayNumber;
+let sessionMath = "";
+let endValue = "";
+let number1 = document.querySelector(".numb");
+let number2 = document.querySelector(".numb");
+let operator = document.querySelector(".oper");
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+
+function updateDisplay(value) {
+    display.value = String(value);
+    if (value == "") {
+        display.value = "";
+    }
+}
+
+buttons.forEach(
+    (button) => {
+        button.addEventListener("click", () => {
+            if (button.classList.contains("numb") || button.classList.contains("spec")) {
+                sessionMath += button.textContent;
+                updateDisplay(sessionMath);
+            } else if (button.classList.contains("oper")) {
+                if (number1 && operator && sessionMath) {
+                    operator = button.textContent;
+                    number1 = sessionMath;
+                    sessionMath = "";
+                    updateDisplay(operator);
+                } else {
+                    operator = button.textContent;
+                    number1 = sessionMath;
+                    sessionMath = "";
+                }
+            } else if (button.classList.contains("equal")) {
+                if (number1 && operator && sessionMath) {
+                    number2 = sessionMath;
+                    let result = operate(parseFloat(number1), parseFloat(number2), operator)
+                    updateDisplay(result);
+                    number1 = "";
+                    operator = "";
+                    sessionMath = "";
+                    number2 = "";
+                }
+            } else if (button.classList.contains("clear")) {
+                sessionMath = "";
+                number1 = "";
+                number2 = "";
+                operator = "";
+                updateDisplay("");
+            }
+        })
+    });
 
 function add(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+    return num1 / num2;
 }
 
 function operate(num1, num2, op) {
     if (op == "+") {
-        return add(num1, num2);
+        endValue = add(num1, num2);
+        return endValue;
     } else if (op == "-") {
-        return subtract(num1, num2);
+        endValue = subtract(num1, num2);
+        return endValue;
     } else if (op == "*") {
-        return multiply(num1, num2)
+        endValue = multiply(num1, num2)
+        return endValue;
     } else if (op == "/") {
-        return divide(num1, num2)
+        endValue = divide(num1, num2)
+        return endValue;
     }
 }
-
-console.log(operate(6, 18, "+"));
